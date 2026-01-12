7e50af961522a53eac3739499c8f1869fb555664
macsunjohn <marksunjohn@gmail.com>
2026-01-12 02:41:41 +0100

    exercise3 completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,30 +5,45 @@ class Program
     static void Main(string[] args)
 
     {
-        // Ask for the magic number
-        Console.Write("What is the magic number? ");
-        int magicNumber = int.Parse(Console.ReadLine());
+        string playAgain = "yes";
 
-        int guess = -1;
-
-        // Loop until the correct guess
-        while (guess != magicNumber)
+        // Loop to allow playing multiple games (stretch challenge)
+        while (playAgain.ToLower() == "yes")
         {
-            Console.Write("What is your guess? ");
-            guess = int.Parse(Console.ReadLine());
+            // Generate a random magic number between 1 and 100
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
 
-            if (guess < magicNumber)
+            int guess = -1;
+            int guessCount = 0;
+
+            // Loop until the user guesses the magic number
+            while (guess != magicNumber)
             {
-                Console.WriteLine("Higher");
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
             }
-            else if (guess > magicNumber)
-            {
-                Console.WriteLine("Lower");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it!");
-            }
+
+            // Display number of guesses (stretch challenge)
+            Console.WriteLine($"It took you {guessCount} guesses.");
+
+            // Ask if the user wants to play again
+            Console.Write("Do you want to play again? (yes/no) ");
+            playAgain = Console.ReadLine();
         }
     }
 
