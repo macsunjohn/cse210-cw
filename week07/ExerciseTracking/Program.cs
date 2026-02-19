0170875f896dfb90dad3ff4f761cce164f2f5187
macsunjohn <marksunjohn@gmail.com>
2026-02-19 17:09:27 +0100

    commit to excercise tracking

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running("03 Nov 2025", 30, 4.8),
+            new Cycling("03 Nov 2025", 40, 15.0),
+            new Swimming("03 Nov 2025", 25, 30)
+        };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
